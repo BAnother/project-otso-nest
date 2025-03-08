021c64cc0a0a2f80ba46ffb22a2153d4b8bd6536
BAnother <sonicyshadow20@gmail.com>
2025-03-08 17:41:51 -0600

    JWTs Service Global

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from "bcrypt"
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class AuthService {
+ constructor(@InjectRepository(User) private userRepository: Repository<User>){}
+  registerUser(createUserDto: CreateUserDto){
+    createUserDto.userPassword = bcrypt.hashSync(createUserDto.userPassword, 5)
+    return this.userRepository.save(createUserDto)
+ }
+
+ async loginUser(createUserDto: CreateUserDto){
+  const user = await this.userRepository.findOne({
+    where: {
+      userEmail: createUserDto.userEmail
+    }
+  })
+
+  //Preguntar
+  if (!user) {
+    throw NotFoundException
+  }
+  //
+  
+  const match = await bcrypt.compare(createUserDto.userPassword, user.userPassword)
+  if (!match) throw new UnauthorizedException("No estas Autorizado")
+  const token = jwt.sign(JSON.stringify(user), "SECRET KEY");  
+  return token
+ }
+}
